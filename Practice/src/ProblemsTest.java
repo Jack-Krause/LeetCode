07fcbb1702934091b97c09d7464d52b5a528c240
Jack Krause <jmkrause@iastate.edu>
2023-11-15 23:07:41 -0600

    test cases

--- Practice/src/ProblemsTest.java
+++ Practice/src/ProblemsTest.java
@@ -8,13 +8,42 @@ class ProblemsTest {
     void sortVowelsTestA() {
         String vString1 = "lEetcOde";
         String vString2 = "lYmpH";
+
         String vE1 = "lEOtcede";
         String vE2 = "lYmpH";
 
         String out = Problems.sortVowels(vString1);
-        assertEquals(vE1, out);
         String out2 = Problems.sortVowels(vString2);
-        assertEquals(vE2, out2);
+
+        assertAll("Given tests",
+                () -> assertEquals(vE1, out),
+                () -> assertEquals(vE2, out2)
+        );
+    }
+
+    @Test
+    void sortVowelsTestB() {
+        String vString1 = "";
+        String vString2 = "ttttttt";
+        String vString3 = "cccccccccua";
+        String vString4 = "UUUOOOIIIEEEAAA";
+
+        String vE1 = "";
+        String vE2 = "ttttttt";
+        String vE3 = "cccccccccau";
+        String vE4 = "AAAEEEIIIOOOUUU";
+
+        String out1 = Problems.sortVowels(vString1);
+        String out2 = Problems.sortVowels(vString2);
+        String out3 = Problems.sortVowels(vString3);
+        String out4 = Problems.sortVowels(vString4);
+
+        assertAll("Edge cases",
+                () -> assertEquals(vE1, out1),
+                () -> assertEquals(vE2, out2),
+                () -> assertEquals(vE3, out3),
+                () -> assertEquals(vE4, out4)
+                );
     }
 
 
